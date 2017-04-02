75cc626d5ac1854c6876af854bf237b5e3f16282
Roshan <rcroshan63@gmail.com>
2017-04-02 08:40:23 +0545

    updated random quote js

--- random-quote.js
+++ random-quote.js
@@ -4,8 +4,12 @@ $("#btnQuote").on("click", function() {
 		url: "http://api.forismatic.com/api/1.0/?",
 		data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
 		success: function (response) {
-			$(".quote").html( response.quoteText );
-			$(".author").html( response.quoteAuthor ); 
+			putContent( response ); 
 		}
 	});
 });
+
+function putContent( response ) {
+	$(".quote").html( response.quoteText );
+	$(".author").html( response.quoteAuthor );
+}
